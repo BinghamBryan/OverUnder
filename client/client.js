92481b483a26aeed81dc4da7bd3a9859a7c70df6
Bryan Bingham <binghambryan@gmail.com>
2013-11-06 22:38:20 -0800

    Added insert question and add/update answers

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
-Template.page.greeting = function () {
-  return "Welcome to overunder.";
-};
+Meteor.subscribe('questions');
 
+//////////////////////////////////////////////////////////////////////////////
+// Page Template
 Template.page.helpers({
 
 });
@@ -10,10 +10,68 @@ Template.page.questions = function(){
   return Questions.find();
 }
 
+Template.page.getNumberOfUnders = function(){
+	return numberOfUnders(this);
+}
+
+Template.page.getNumberOfOvers = function(){
+	return numberOfOvers(this);
+}
+
 Template.page.events({
-  'click input' : function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+  'click .answer_over': function () {
+    Meteor.call("answer", this._id, "over");
+    return false;
+  },
+  'click .answer_under': function () {
+    Meteor.call("answer", this._id, "under");
+    return false;
+  },
+  'click .remove': function () {
+    Questions.remove(this._id);
+    return false;
+  },
+  'click .add': function () {
+  	openCreateDialog()
+  	return false;
   }
 });
+
+///////////////////////////////////////////////////////////////////////////////
+// Create Party dialog
+
+var openCreateDialog = function () {
+  Session.set("createError", null);
+  Session.set("showCreateDialog", true);
+};
+
+Template.page.showCreateDialog = function () {
+  return Session.get("showCreateDialog");
+};
+
+Template.createDialog.events({
+  'click .save': function (event, template) {
+    var text = template.find(".text").value;
+    var expires = template.find(".expires").value;
+
+    if (text.length && expires.length) {
+      var id = createQuestion({
+        text: text,
+        expires: expires
+      });
+
+      Session.set("showCreateDialog", false);
+    } 
+    else {
+      Session.set("createError", "It needs a title and a description, or why bother?");
+    }
+  },
+
+  'click .cancel': function () {
+    Session.set("showCreateDialog", false);
+  }
+});
+
+Template.createDialog.error = function () {
+  return Session.get("createError");
+};
